27af85f26236447bb192949c6244c465cdc2ac9d
Norbert Pabian <norbert.pabian@gmail.com>
2015-06-25 05:23:32 +0200

    New theme

---
+++ wp-content/themes/ckip/includes/woocommerce.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Woocommerce functions and definitions
+ *
+ * @package nps
+ */
+
+function woocommerce_cart_link() {
+    global $woocommerce;
+    ?>
+    <a href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'npstheme'); ?>" class="cart-parent">
+        <!--
+            <span class="contents"><?php echo $woocommerce->cart->get_cart_contents_count() ?></span>
+            <span class="total"><?php echo $woocommerce->cart->get_cart_total() ?></span>
+        -->
+        <i class="glyphicon glyphicon-shopping-cart"></i>
+    </a>
+    <?php
+}
+
+function woocommerce_my_account_link() {
+     if ( is_user_logged_in() ) { ?>
+        <a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('My Account','npstheme'); ?>"><?php _e('My Account','npstheme'); ?></a>
+     <?php } 
+     else { ?>
+        <a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php _e('Login / Register','npstheme'); ?>"><?php _e('Login / Register','npstheme'); ?></a>
+     <?php }
+}
