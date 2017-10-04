96b803468c4bed29b4058122837d552e718e3b29
Norbert Pabian <norbert.pabian@xing.com>
2017-10-04 16:44:32 +0200

    Update cleanup

--- wp-content/themes/nps/woocommerce/cart/cart-empty.php
+++ wp-content/themes/nps/woocommerce/cart/cart-empty.php
@@ -2,17 +2,25 @@
 /**
  * Empty cart page
  *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-empty.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.0.0
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+    exit; // Exit if accessed directly
 }
 
 wc_print_notices();
-
 ?>
 
 <p class="alert alert-info cart-empty"><?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?></p>
