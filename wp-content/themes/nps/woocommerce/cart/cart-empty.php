92b2d3c9f6dc3f60eb6f8c5ea7ea28e8a81cebe2
Norbert Pabian <norbert.pabian@xing.com>
2017-10-13 10:33:48 +0200

    Style improvements

--- wp-content/themes/nps/woocommerce/cart/cart-empty.php
+++ wp-content/themes/nps/woocommerce/cart/cart-empty.php
@@ -25,8 +25,6 @@ wc_print_notices();
 
 <p class="alert alert-info cart-empty"><?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?></p>
 
-<?php do_action( 'woocommerce_cart_is_empty' ); ?>
-
 <p class="return-to-shop">
     <a class="button wc-backward" href="<?php echo get_site_url().'/'.Tribe__Events__Main::instance()->getOption( 'eventsSlug', 'events' ); ?>"><?php _e( 'Return To Events', 'nps' ) ?></a>
 </p>
