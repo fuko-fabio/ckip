7369480bcf37bd40bc56a26e8497cfa16f6db94c
Norbert Pabian <norbert.pabian@xing.com>
2017-10-13 11:11:39 +0200

    Cleanup

--- wp-content/themes/nps/functions.php
+++ wp-content/themes/nps/functions.php
@@ -182,7 +182,7 @@ function nps_scripts() {
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( 'nps-keyboard-image-navigation', get_template_directory_uri() . '/includes/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
-    wp_enqueue_style( 'nps-extended-style', get_template_directory_uri().'/includes/css/nps.css', array(), '1.2.8' );
+    wp_enqueue_style( 'nps-extended-style', get_template_directory_uri().'/includes/css/nps.css', array(), '1.2.10' );
     wp_enqueue_style( 'nps-search-style', get_template_directory_uri().'/includes/css/search.css', array(), '1.0.2' );
     wp_enqueue_style( 'nps-style', get_stylesheet_uri(), array(), '1.2.0'  );
 }
--- wp-content/themes/nps/head.php
+++ wp-content/themes/nps/head.php
@@ -14,7 +14,7 @@ global $woocommerce;
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="profile" href="https://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
 </head>
